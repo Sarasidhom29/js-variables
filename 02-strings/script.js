37c10d821c1fc4d13401c5538d5a5d9111cc934e
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-03-20 16:25:48 +0100

    svolto esercizio 2 in strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,11 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 15;
+let text = 'Ciao a tutti';
+let bool = false;
+console.log(number, text, bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
